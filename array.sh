6603193f063c3d4769b73a2ee89e67a1ec7040a4
siva <pesiinfosys@gmail.com>
2024-02-08 12:22:33 +0000

    adding sample.sh

--- array.sh
+++ array.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 echo "my array deatils"
-declare -a names=("siva" "sri" "harika" "pesi")
-echo "1--> ${names[0]}"
-echo "2--> ${names[1]}"
-echo "3--> ${names[2]}"
-echo "4--> ${names[3]}"
-
-echo "full array using @ : ${names[@]}"
-echo "full array using * : ${names[*]}"
+PERSONS=("siva" "sri" "pesi")
+echo "first persion: ${PERSONS[0]}"
