098b5210ae14246038df9ab752c974e01ee8ba4e
siva <pesiinfosys@gmail.com>
2024-02-08 13:08:27 +0000

    adding condition.sh

---
+++ condition.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Enter the value less than or equal to 10 :"
+read x
+if [ $x <= 10 ]
+then
+    echo "you entered value <=10...Nice"
+else
+    echo "you entered value >=10...Try Again"
+fi
